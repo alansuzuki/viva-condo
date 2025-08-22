521b47f8538a8a83d1fc4c6bafc38ed39bfe8f51
Carequinha <carequinha@gmail.com>
2025-08-22 19:48:57 -0300

    feat: adiciona componente azul centralizado na Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,32 @@
 "use client";
 
 type Morador = {
-  primeiroNome: string,
-  sobreNome:string,
-}
+  primeiroNome: string;
+  sobreNome: string;
+};
 
 export default function Home() {
-
-function formatarNomeMorador (morador:Morador) {
-  return morador.primeiroNome + ' ' + morador.sobreNome;
-}
-
-function obterSaudacao (morador:null | Morador) {
-  if (morador){
-    return <span>Olá, {formatarNomeMorador(morador)}!</span>;
+  function formatarNomeMorador(morador: Morador) {
+    return morador.primeiroNome + " " + morador.sobreNome;
   }
-  return <span>Olá, Estranho!!!</span>;
-}
 
-const morador = {
-  primeiroNome: 'Sandro',
-  sobreNome:'Pereira'
-};
+  function obterSaudacao(morador: null | Morador) {
+    if (morador) {
+      return <span>Olá, {formatarNomeMorador(morador)}!</span>;
+    }
+    return <span>Olá, Estranho!!!</span>;
+  }
+
+  const morador = {
+    primeiroNome: "Sandro",
+    sobreNome: "Pereira",
+  };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className= "text-2xl font-semibold">{obterSaudacao (null)} </h1>
+    <div id="principal" className="min-h-screen flex items-center justify-center bg-black">
+      <div id="componente-azul"className="card-azul">
+        <h1 id = "name" className="text-2xl font-semibold">{obterSaudacao(null)}</h1>
+      </div>
     </div>
   );
-};
+}
